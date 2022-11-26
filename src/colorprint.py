c2748b260aaf662dd0aa71e9d1ca3b1d3a372a7a
Esgrove <esgrove@outlook.com>
2022-11-26 20:13:20 +0200

    sort colors alphabetically

--- src/colorprint.py
+++ src/colorprint.py
@@ -14,13 +14,13 @@ colorama.init(strip=False, convert=False)
 class Color:
     """Interface for Colorama colors."""
 
-    red = colorama.Fore.RED
-    green = colorama.Fore.GREEN
-    yellow = colorama.Fore.YELLOW
     blue = colorama.Fore.BLUE
-    magenta = colorama.Fore.MAGENTA
     cyan = colorama.Fore.CYAN
+    green = colorama.Fore.GREEN
+    magenta = colorama.Fore.MAGENTA
+    red = colorama.Fore.RED
     white = colorama.Fore.WHITE
+    yellow = colorama.Fore.YELLOW
 
 
 def get_color(text: str, color=Color.white, bold=False) -> str:
